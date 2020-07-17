92a30fb3cb3617ee23be6207a0930fef66c22bfa
Shashank Agarwal <63952954+shashank80-0@users.noreply.github.com>
2020-07-17 14:47:39 +0530

    Add files via upload

--- Application.py
+++ Application.py
@@ -1,101 +1,95 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail, Message
 app = Flask(__name__)
 
-app.config ['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://root:12345@localhost:3308/new_db')
+app.config ['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://root:12345@localhost:3308/my_db')
 app.config ['SQLALCHEMY_ECHO'] = False
-app.config['FLASK_ENV'] = 'development'
+app.config ['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config ['MAIL_PORT'] = 465
+app.config ['MAIL_USERNAME'] = 'photosbackupshashank@gmail.com'
+app.config ['MAIL_PASSWORD'] = 'sa@90279808'
+app.config ['MAIL_USE_TLS'] = False
+app.config ['MAIL_USE_SSL'] = True
+
 
 db=SQLAlchemy(app)
 
 
-class designation(db.Model):
-    role_id = db.Column(db.String(2),primary_key=True)
+class role(db.Model):
+    role_id = db.Column(db.String(5),primary_key=True)
     role_name = db.Column(db.String(45),nullable=False,unique=True)
-    
+
+class employee(db.Model):
+    emp_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    first_name = db.Column(db.String(99),nullable=False)
+    last_name = db.Column(db.String(99),nullable=False)
+    birth_date = db.Column(db.DateTime,nullable=False)
+    email_addr = db.Column(db.String(99),nullable=False,unique=True)
+    passwd = db.Column(db.String(99),nullable=False)    
+
+class designation(db.Model):
+    id_designation = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    emp_id = db.Column(db.Integer,db.ForeignKey(employee.emp_id), primary_key =True,nullable=False)
+    designation_emp_id = db.relationship('employee', backref = db.backref('designation_emp_id'), lazy=False)
+    role_id =db.Column(db.String(5),db.ForeignKey(role.role_id), nullable=False)
+    designation_role_id = db.relationship('role', backref =db.backref('designation_role_id'), lazy=False)
 
 class technology(db.Model):
-    skill_id = db.Column(db.String(2),primary_key=True,nullable=False)
-    skill_name = db.Column(db.String,nullable=False,unique=True)
+    skill_id = db.Column(db.String(5),primary_key=True,nullable=False)
+    skill_name = db.Column(db.String(99),nullable=False,unique=True)
 
 class address(db.Model):
-    city_id = db.Column(db.String(45),primary_key=True,nullable=False)
+    city_id = db.Column(db.Integer,primary_key=True,nullable=False, autoincrement=True)
     city_name = db.Column(db.String(45),nullable=False)
     state_name = db.Column(db.String(45),nullable=False)
-    pincode_num = db.Column(db.Integer,nullable=False)
-
-
-class staff(db.Model):
-    emp_id = db.Column(db.String(11), primary_key=True, nullable=False)
-    first_name = db.Column(db.String(45),nullable=False)
-    last_name = db.Column(db.String(45),nullable=False)
-    birth_date = db.Column(db.DateTime,nullable=False)
-    passwd = db.Column(db.String(45),nullable=False)
-    designation_role_id = db.Column(db.String(2),db.ForeignKey(designation.role_id),nullable=True)
-    roleId = db.relationship('designation', backref = db.backref('roleId'), lazy = True, uselist='False')
-    
+    pincode_num = db.Column(db.String(6),nullable=False)
 
 class attendance(db.Model):
-    staff_emp_id = db.Column(db.String(11),db.ForeignKey(staff.emp_id), primary_key=True, nullable=True)
-    empId = db.relationship('staff',backref=db.backref('empId'),lazy=True)
     on_date = db.Column(db.DateTime,primary_key = True,nullable=False)
+    emp_id = db.Column(db.Integer,db.ForeignKey(employee.emp_id), primary_key=True, nullable=True)
+    attendance_emp_id = db.relationship('employee',backref=db.backref('attendance_emp_id'),lazy=False)
     date_status = db.Column(db.String(1),nullable=False)
 
-
-
 class attribute(db.Model):
-    attributecol = db.Column(db.Integer,primary_key=True,nullable=False)
-    staff_emp_id = db.Column(db.String(11),db.ForeignKey(staff.emp_id), nullable=False)
-    #empId = db.relationship('staff',backref=db.backref('empId'),lazy=True)
-    technology_skill_id = db.Column(db.String(2),db.ForeignKey(technology.skill_id),nullable=False)
-    skillId = db.relationship('technology',backref = db.backref('skillId'),lazy=True,uselist=False)
-
+    emp_id = db.Column(db.Integer,db.ForeignKey(employee.emp_id),primary_key=True, nullable=False)
+    attribute_emp_id = db.relationship('employee',backref=db.backref('attribute_emp_id'),lazy=False)
+    skill_id = db.Column(db.String(5),db.ForeignKey(technology.skill_id),primary_key=True,nullable=False)
+    attribute_skill_id = db.relationship('technology',backref = db.backref('attribute_skill_id'),lazy=False)
+    skill_level = db.Column(db.Integer,nullable=False)
 
 class contact(db.Model):
-    staff_emp_id = db.Column(db.String(11),db.ForeignKey(staff.emp_id),primary_key = True, nullable=False)
-    #empId = db.relationship('staff', backref = db.backref('empId'), lazy=True,uselist=False)
-    phone_num = db.Column(db.String(10),nullable=False,unique=True)
-    email_addr = db.Column(db.String(45),nullable=False,unique=True)
-    home_addr = db.Column(db.String(99),nullable=False)
-    address_city_id = db.Column(db.String(45),db.ForeignKey(address.city_id),nullable=False)    
-    cityId = db.relationship('address', backref=db.backref('cityId'), lazy=True,uselist=False)
-
-
-
-'''
-def __init__ (emp_id,first_name,last_name,birth_date,passwd,designation_role_id):
-    self.emp_id=emp_id
-    self.first_name=first_name
-    self.last_name=last_name
-    self.birth_date=birth_date
-    self.passwd=passwd
-    self.designation_role_id=designation_role_id
-'''
+    emp_id = db.Column(db.Integer,db.ForeignKey(employee.emp_id),primary_key = True, nullable=False)
+    contact_emp_id = db.relationship('employee', backref = db.backref('contact_emp_id'), lazy=False,uselist=False)
+    phone_num = db.Column(db.String(10),nullable=False,unique=True)    
+    home_addr = db.Column(db.String,nullable=False)
+    city_id = db.Column(db.Integer,db.ForeignKey(address.city_id),nullable=False)    
+    contact_city_id = db.relationship('address', backref=db.backref('contact_city_id'), lazy=False,uselist=False)
 
 
 db.create_all()
+
 '''
-user = staff.query.filter_by(first_name='Creed').first()
+
+user = employee.query.filter_by(first_name='Niharika').first()
 print(user.last_name)
-print(user.roleId)
 print(user.emp_id)
-print(user.designation_role_id)
-userContact = contact.query.filter_by(staff_emp_id=user.emp_id).first()
-#print(userContact.empId)
-print(userContact.cityId)
-#print(userContact.city_name)
+userContact = contact.query.filter_by(emp_id=user.emp_id).first()
+print(userContact.city_id)
 print(userContact.home_addr)
 print(userContact.phone_num)
-userAtt = attribute.query.filter_by(staff_emp_id = user.emp_id).all()
+userAtt = attribute.query.filter_by(emp_id = user.emp_id).all()
 for att in userAtt:
-    tech = technology.query.filter_by(skill_id = att.technology_skill_id).first()
+    tech = technology.query.filter_by(skill_id = att.skill_id).first()
     print(tech.skill_name)
-userAttendance = attendance.query.filter_by(staff_emp_id = user.emp_id).all()
+userAttendance = attendance.query.filter_by(emp_id = user.emp_id).all()
 for item in userAttendance:
     print (item.on_date)
     print (item.date_status)
+
 '''
+
 
 
 @app.route('/login', methods=['POST','GET'])
@@ -106,7 +100,7 @@ def login():
     elif request.method == 'POST':
         employeeId = request.form['user']
         password = request.form['pass']
-        user = staff.query.filter_by(emp_id = employeeId).first()
+        user = employee.query.filter_by(emp_id = employeeId).first()
         if(user is None):
             error = """User does not exist!"""
             return render_template('login.html', error = error)
@@ -142,36 +136,80 @@ def signup():
         elif(contact.query.filter_by(phone_num = phoneNum).first() is not None):
             error = """Phone number is already registered!"""
             return render_template('signup.html',error = error)
-        elif(contact.query.filter_by(email_addr = email).first() is not None):
+        elif(employee.query.filter_by(email_addr = email).first() is not None):
             error = """Email address is already registered!"""
             return render_template('signup.html',error = error)
         else:
-            print(dob)
-            one = str(firstName[0].upper())
-            two = str(dob[2:4])
-            three = str(dob[5:7])
-            four = (dob[8:10])
-            five = (lastName[0:2].upper())
-            eIdFormat = one+two+'-'+three+'-'+four+five
-            print(eIdFormat)
-            user = staff(emp_id = eIdFormat,
+            user = employee(
                          first_name = firstName,
                          last_name = lastName,
                          birth_date = dob,
-                         passwd = password,
-                         designation_role_id = 'UN')
+                         email_addr = email,
+                         passwd = password
+                         )
             db.session.add(user)
             db.session.commit()
-            return redirect(url_for('start_login'))
+            return redirect(url_for('login'))
 
 @app.route('/home/<output>')
 def home(output):
     return render_template('home.html',name = output)
 
+@app.route('/forget', methods=['POST','GET'])
+def forget():
+    error = None
+    if request.method == 'GET': 
+        return render_template('forget.html')
+    elif request.method == 'POST':
+        email = request.form['email']
+        if(email == ''):
+            error = """Email address cannot be empty!"""
+            return render_template('forget.html', error = error)
+        elif(employee.query.filter_by(email_addr=email).first() is None):
+            error = """Email address is incorrect or does not exist!"""
+            return render_template('forget.html', error=error)
+        else:
+            mail = Mail(app)
+            msg = Message('Password reset', sender = 'photosbackup@gmail.com', recipients = [email])
+            msg.body = """Please follow this link to reset your password!
+                            http://localhost:5000/reset
+                            Thank you!"""
+            mail.send(msg)
+            error = """Reset password link has been sent by email!"""
+            return (render_template('forget.html', error = error))
+
+@app.route('/reset', methods=['POST','GET'])
+def reset():
+    error = None
+    if request.method == 'GET':
+        return render_template('reset.html')
+    elif request.method == 'POST':
+        email = request.form['email']
+        password = request.form['pass']
+        confirmPassword = request.form['confirmPass']
+        if(email == '' or password == '' or confirmPassword == ''):
+            error = """One or more fields are empty!"""
+            return render_template('reset.html',error = error)
+        elif(password != confirmPassword):
+            error = """Passwords do not match!"""
+            return render_template('reset.html',error = error)
+        elif(employee.query.filter_by(email_addr=email).first() is None):
+            error = """Email address is incorrect or does not exist!"""
+            return render_template('reset.html', error=error)
+        else:
+            user = employee.query.filter_by(email_addr = email).first()
+            user.passwd = password
+            db.session.commit()
+            error = """Password has been successfully changed!"""
+            return render_template('reset.html',error = error)
+            
+            
+             
+        
+    
+
 if __name__ == '__main__':
     app.run()
-
-
 
 
 
